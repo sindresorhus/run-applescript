0c2e09addecb2384efe3bc310c0afee033586d86
Kevin Martensson <kevinmartensson@gmail.com>
2015-05-06 18:42:07 +0200

    Throw error if used on another OS than OS X

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 var execFile = require('child_process').execFile;
 
 module.exports = function (str, cb) {
+	if (process.platform !== 'darwin') {
+		throw new Error('Only OS X systems are supported');
+	}
+
 	execFile('osascript', ['-e', str], function (err, stdout) {
 		if (err) {
 			cb(err);
