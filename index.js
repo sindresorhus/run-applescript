3642b50f634b3250c27264a9044247294e8e9469
Sindre Sorhus <sindresorhus@gmail.com>
2016-06-14 15:11:30 +0200

    OS X → macOS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const execa = require('execa');
 
 module.exports = str => {
 	if (process.platform !== 'darwin') {
-		return Promise.reject(new Error('Only OS X is supported'));
+		return Promise.reject(new Error('macOS only'));
 	}
 
 	return execa.stdout('osascript', ['-e', str]);
