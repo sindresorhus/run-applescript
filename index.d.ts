3cc7bc5a38a7873782a85cea3a80f3183d828c02
Gary King <garyking@gmail.com>
2019-10-24 12:22:20 -0400

    fix comments format

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,18 @@
 /**
- * Run Applescript
- *
- * @param script The script to run.
- * @returns The script result.
- */
+Run Applescript
+
+@param script The script to run.
+@returns The script result.
+*/
 declare function runApplescript(script: string): Promise<string>;
 
 declare namespace runApplescript {
 	/**
-	 * Run Applescript sync
-	 *
-	 * @param script The script to run.
-	 * @returns The script result.
-	 */
+	Run Applescript sync
+
+	@param script The script to run.
+	@returns The script result.
+	*/
 	function sync(script: string): string;
 }
 
