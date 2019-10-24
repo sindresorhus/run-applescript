94e777c57a4fa60e1a41dff4486fefd4aa70de2c
Gary King <garyking@gmail.com>
2019-10-24 12:23:38 -0400

    add examples

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,18 @@
 /**
 Run Applescript
 
+@example
+```
+const runApplescript = require('run-applescript');
+
+(async () => {
+	const result = await runApplescript('return "unicorn"');
+
+	console.log(result);
+	//=> 'unicorn'
+})();
+```
+
 @param script The script to run.
 @returns The script result.
 */
@@ -9,6 +21,16 @@ declare function runApplescript(script: string): Promise<string>;
 declare namespace runApplescript {
 	/**
 	Run Applescript sync
+
+	@example
+	```
+	const runApplescript = require('run-applescript');
+
+	const result = runApplescript.sync('return "unicorn"');
+
+	console.log(result);
+	//=> 'unicorn'
+	```
 
 	@param script The script to run.
 	@returns The script result.
